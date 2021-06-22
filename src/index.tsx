91f3fbc7e8264e7532904dd5d9bbfa167020284e
wzq <zhiqiang.wang@yff.com>
2021-06-22 17:06:26 +0800

    chore: 升级eslint配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './styles/common.scss';
 // import App from './App';
 import routes from './router';
 import { renderRoutes } from 'react-router-config';
--- src/views/books/index.tsx
+++ src/views/books/index.tsx
@@ -1,7 +1,12 @@
+import { useHistory } from 'react-router-dom';
 const Books = () =>{
+  const history = useHistory();
+  const goBack = () => {
+    history.goBack();
+  };
   return (
     <div>
-      <h1>Books</h1>
+      <h1 onClick={() => goBack()}>Books</h1>
     </div>
   );
 };
