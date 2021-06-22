e73603bac2212eca82071bc7fd9a51845bdccd64
wzq <zhiqiang.wang@yff.com>
2021-06-22 18:51:17 +0800

    feat: useState

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,14 +1,23 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import './index.scss';
 
 const Home = () =>{
   const history = useHistory();
-  const handleClick = (path: string): void => {
+  const handleClick = (path: string) => {
     history.push(`/${path}`);
+  };
+  const [count, setCount] = useState(0);
+  const setCounts = () => {
+    setCount(count + 1);
   };
   return (
     <div className="home">
       <h1>Home</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCounts()}>
+        Click me
+      </button>
       <ul className="home-ul">
         <li onClick={() => handleClick('about')}>About</li>
         <li onClick={() => handleClick('books')}>Books</li>
