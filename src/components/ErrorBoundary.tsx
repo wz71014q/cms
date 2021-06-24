e47457e2e909be130c484dc3907ba4918fa8a290
wzq <zhiqiang.wang@yff.com>
2021-06-24 14:44:23 +0800

    feat: alias'@'&errorBoundary

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ErrorBoundary:React.FC = ({children}) => {
   // 目前还没有对应getDerivedStateFromError生命周期的hook，所以函数式组件无法作错误边界
   // return <h1>Something went wrong.</h1>;
-  return <div>{children}</div>;
+  return <div className="error-boundary">{children}</div>;
 };
 
 ErrorBoundary.propTypes = {
