83e12571f890865a7ba655b418d0c93da7d018d9
wzq <zhiqiang.wang@yff.com>
2021-06-24 15:12:52 +0800

    feat: useEffect

--- src/views/aboutChild/index.tsx
+++ src/views/aboutChild/index.tsx
@@ -1,6 +1,6 @@
 import { RouteConfigComponentProps } from 'react-router-config';
 import { match, useRouteMatch } from 'react-router-dom';
-import { ReactElement, useContext } from 'react';
+import { ReactElement, useContext, useEffect } from 'react';
 import { AppContext } from '../about';
 interface RouteParams extends RouteConfigComponentProps{
   someProp?: string,
@@ -15,6 +15,12 @@ interface MatchParams extends match{
 const AboutChild = (props: RouteParams): ReactElement =>{
   const matchParams: MatchParams = useRouteMatch();
   const appContext = useContext(AppContext);
+  useEffect(() => {
+    console.log('aboutid依赖项变更，触发新effect', matchParams.params.aboutid);
+    return () => {
+      console.log('组件重新渲染，清除旧effect', matchParams.params.aboutid);
+    };
+  }, [matchParams.params.aboutid]);
   return (
   <div>
     <h3>Grand Child</h3>
