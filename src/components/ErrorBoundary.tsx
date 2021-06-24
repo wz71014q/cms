5ec786d3a089c463a9ff5aa3df872061e69e7c29
wzq <zhiqiang.wang@yff.com>
2021-06-24 18:52:27 +0800

    feat: useReducer

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ErrorBoundary:React.FC = ({children}) => {
+const ErrorBoundary:React.FC = (props) => {
   // 目前还没有对应getDerivedStateFromError生命周期的hook，所以函数式组件无法作错误边界
   // return <h1>Something went wrong.</h1>;
-  return <div className="error-boundary">{children}</div>;
+  return props.children as React.ReactElement;
 };
 
 ErrorBoundary.propTypes = {
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,17 @@
 import { useHistory } from 'react-router-dom';
-import { useState, ReactElement } from 'react';
+import { useState, ReactElement, useEffect, useReducer } from 'react';
 import './index.scss';
+
+const reducer = (state: { count: number }, action: { type: string }) => {
+    switch (action.type) {
+      case 'increment':
+        return {count: state.count + 1};
+      case 'decrement':
+        return {count: state.count - 1};
+      default:
+        throw new Error();
+    }
+};
 
 const Home = ():ReactElement =>{
   const history = useHistory();
@@ -11,6 +22,11 @@ const Home = ():ReactElement =>{
   const setCounts = () => {
     setCount(count + 1);
   };
+  const initialState = {count: 0};
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    console.log(`You clicked ${count} times`);
+  }, [count]);
   return (
     <div className="home">
       <h1>Home</h1>
@@ -18,6 +34,11 @@ const Home = ():ReactElement =>{
       <button onClick={setCounts}>
         Click me
       </button>
+      <p>
+        state.count: {state.count}
+      </p>
+      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch({type: 'increment'})}>+</button>
       <ul className="home-ul">
         <li onClick={() => handleClick('about')}>About</li>
         <li onClick={() => handleClick('books')}>Books</li>
